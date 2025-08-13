624299f98e7b2a63631ef2b4309d347f2eaf943a
test <brucemclarenp4@gmail.com>
2025-08-14 08:03:03 +0900

    タグにリンク追加

--- resources/js/pages/PlaceDetail.vue
+++ resources/js/pages/PlaceDetail.vue
@@ -21,9 +21,14 @@
       </div>
 
       <div class="mt-6 flex flex-wrap gap-2">
-        <span v-for="t in p.tags || []" :key="t.slug" class="text-xs px-2 py-1 bg-gray-100 rounded-full">
+        <!-- <span v-for="t in p.tags || []" :key="t.slug" class="text-xs px-2 py-1 bg-gray-100 rounded-full">
           #{{ t.name }}
-        </span>
+        </span> -->
+        <router-link v-for="t in p.tags || []" :key="t.slug"
+          class="text-xs px-2 py-1 bg-gray-100 rounded-full hover:bg-gray-200"
+          :to="{ name:'tag', params:{ locale: $route.params.locale, slug: t.slug } }">
+          #{{ t.name }}
+        </router-link>
       </div>
     </div>
 
