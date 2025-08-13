73ca74da4547d662ded188437090eb66f26ed106
test <brucemclarenp4@gmail.com>
2025-08-14 08:07:09 +0900

    ルーターにプレフィルタ & タグ用ルートを追加

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,14 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const PlaceList = () => import('../pages/PlaceList.vue')
+const PlaceList   = () => import('../pages/PlaceList.vue')
 const PlaceDetail = () => import('../pages/PlaceDetail.vue')
+
+const toList = (locale, query) => ({ name: 'list', params: { locale }, query })
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/ja/places' },
-    { name: 'list', path: '/:locale(ja|en)/places', component: PlaceList },
-    { name: 'detail', path: '/:locale(ja|en)/places/:slug', component: PlaceDetail },
+
+    // 一覧・詳細
+    { name: 'list',   path: '/:locale(ja|en)/places',            component: PlaceList },
+    { name: 'detail', path: '/:locale(ja|en)/places/:slug',      component: PlaceDetail },
+
+    // ★ プレフィルタ直リンク（→ 一覧にクエリ付きでリダイレクト）
+    { path: '/:locale(ja|en)/castles/top100',
+      redirect: to => toList(to.params.locale, { type: 'castle', top100: 1 }) },
+    { path: '/:locale(ja|en)/castles/top100-continued',
+      redirect: to => toList(to.params.locale, { type: 'castle', top100c: 1 }) },
+    { path: '/:locale(ja|en)/castles/others',
+      redirect: to => toList(to.params.locale, { others: 1 }) },
+    { path: '/:locale(ja|en)/cultural-properties',
+      redirect: to => toList(to.params.locale, { type: 'cultural_property' }) },
+
+    // ★ タグページ
+    { name: 'tags-index', path: '/:locale(ja|en)/tags', component: () => import('../pages/TagsIndex.vue') },
+    { name: 'tag', path: '/:locale(ja|en)/tags/:slug',
+      redirect: to => toList(to.params.locale, { tags: to.params.slug }) },
   ],
 })
 
