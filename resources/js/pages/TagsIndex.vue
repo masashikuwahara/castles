80e4eca9ea1f311e2a773d6b5dda86c7f5d0b5cc
test <brucemclarenp4@gmail.com>
2025-08-31 00:56:44 +0900

    文化財タグを表示

--- resources/js/pages/TagsIndex.vue
+++ resources/js/pages/TagsIndex.vue
@@ -5,7 +5,7 @@
     <div v-else class="flex flex-wrap gap-2">
       <router-link v-for="t in tags" :key="t.slug"
         class="px-3 py-1 rounded-full border hover:bg-gray-50 text-sm"
-        :to="rl({ name:'tag' })">
+        :to="rl({ name:'list', query:{ tags: t.slug } })">
         #{{ t.name }}
       </router-link>
     </div>
@@ -13,13 +13,15 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import { useLocaleRoute } from '../composables/useLocaleRoute'
 import { listTags } from '../lib/api'
 
 const route = useRoute()
 const tags = ref([])
 const loading = ref(true)
+const { rl, loc } = useLocaleRoute()
 
 onMounted(async () => {
   const { data } = await listTags(route.params.locale)
